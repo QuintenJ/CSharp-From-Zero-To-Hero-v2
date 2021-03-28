2845b9c4fa5eb30bb132c1af1fdf1f9330add15e
Quinten <me@quintenjames.com>
2021-03-28 12:11:36 -0600

    Initial Commit to homework branch

---
+++ RecipeApp/RecipeApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RecipeApp
+{
+    
+    //Here is async list of standard cooking units:
+    //  Cup 0.24 1
+    //  Gallon 3.79 1
+    //  Fluid Ounce 29.57 ml
+    //  Pint 0.47 1
+    //  Quart 0.95 1
+    //  Tablespoon 14.79 ml
+    //  Teaspoon 4.93 ml
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
